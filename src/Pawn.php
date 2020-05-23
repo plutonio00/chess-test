63cc1b34fe86ef0cb4dc8a1964b9ee3241e36949
viktor_kozhinov <victor.kozhinov@westwing.ru>
2020-05-23 16:53:22 +0300

    refactoring check move function

--- src/Pawn.php
+++ src/Pawn.php
@@ -13,22 +13,24 @@ class Pawn extends Figure
      */
     public function checkMove(array $moveData): bool
     {
-        return $this->isBlack() ?
-            $this->checkBlackPawnMove($moveData) : $this->checkWhitePawnMove($moveData);
-    }
-
-    private function checkBlackPawnMove(array $moveData): bool
-    {
         $checkResult = false;
         list($xFrom, $yFrom, $xTo, $yTo, $state) = $moveData;
 
         if ($xFrom === $xTo) {
             $yDiff = $yTo - $yFrom;
 
-            if ($yDiff === -1) {
-                $checkResult = !isset($state[$xTo][$yTo]);
-            } elseif ($yDiff === -2) {
-                $checkResult = !(isset($state[$xFrom][$yFrom - 1]) || isset($state[$xTo][$yTo]));
+            if ($this->isBlack()) {
+                if ($yDiff === -1) {
+                    $checkResult = !isset($state[$xTo][$yTo]);
+                } elseif ($yDiff === -2 && $yFrom === 7) {
+                    $checkResult = !(isset($state[$xFrom][$yFrom - 1]) || isset($state[$xTo][$yTo]));
+                }
+            } else {
+                if ($yDiff === 1) {
+                    $checkResult = !isset($state[$xTo][$yTo]);
+                } elseif ($yDiff === 2 && $yFrom === 2) {
+                    $checkResult = !(isset($state[$xFrom][$yFrom + 1]) || isset($state[$xTo][$yTo]));
+                }
             }
         } elseif (abs(Constants::LETTERS_NUMBERS[$xTo] - Constants::LETTERS_NUMBERS[$xFrom]) === 1) {
             $checkResult = isset($state[$xTo][$yTo]);
@@ -37,24 +39,5 @@ class Pawn extends Figure
         return $checkResult;
     }
 
-    private function checkWhitePawnMove(array $moveData): bool
-    {
-        $checkResult = false;
-        list($xFrom, $yFrom, $xTo, $yTo, $state) = $moveData;
 
-        if ($xFrom === $xTo) {
-            $yDiff = $yTo - $yFrom;
-
-            if ($yDiff === 1) {
-                $checkResult = !isset($state[$xTo][$yTo]);
-            } elseif ($yDiff === 2 && $yFrom === 2) {
-                $checkResult = !(isset($state[$xFrom][$yFrom + 1]) || isset($state[$xTo][$yTo]));
-            }
-        }
-        elseif (abs(Constants::LETTERS_NUMBERS[$xTo] - Constants::LETTERS_NUMBERS[$xFrom]) === 1) {
-            $checkResult = isset($state[$xTo][$yTo]);
-        }
-
-        return $checkResult;
-    }
 }
